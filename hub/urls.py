dc6dd43f083201d9b47c9821b04a5fca258c0f4d
pdeimichei <266090130+pdeimichei@users.noreply.github.com>
2026-09-25 13:11:25 -0400

    Route Azure users without Django login

--- hub/urls.py
+++ hub/urls.py
@@ -1,23 +1,15 @@
-"""
-URL configuration for hub project.
+"""URL configuration for hub."""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('confronto/', include('confronto.urls')),
+    path("", RedirectView.as_view(pattern_name="confronto:risultati", permanent=False)),
+    path("confronto/", include("confronto.urls")),
 ]
+
+# Django admin is a local-development tool. Azure users authenticate with Entra.
+if not settings.AZURE_ENTRA_AUTH:
+    urlpatterns.insert(0, path("admin/", admin.site.urls))
